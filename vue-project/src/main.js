b6810c264dfeeba7992caf8f72eaf1736f1a13f6
tarunkumarchinni <30752090+tarunkumarchinni@users.noreply.github.com>
2020-11-18 22:32:04 +0530

    Add files via upload

---
+++ vue-project/src/main.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from "vue-router"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import BootstrapVue from "bootstrap-vue"
+import Signup from './components/Signup.vue'
+import Loan from './components/Loan.vue'
+import ApplyLoan from './components/ApplyLoan.vue'
+import EditLoan from './components/EditLoan.vue'
+import Home from './components/Home.vue'
+import Register from './components/Register.vue'
+import Login from './components/Login.vue'
+import Vuelidate from 'vuelidate';
+import AccountDetails from './components/AccountDetails'
+import UpdateDetails from './components/UpdateDetails'
+
+// Vue.config.productionTip = false
+Vue.config.silent = true
+Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+
+
+const spaRoutes = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/signup',
+      component: Signup
+    },
+    {
+      path: '/show-loans/:username',
+      component: Loan
+    },
+    {
+      path: '/add-loan/:username',
+      component: ApplyLoan
+    },
+    {
+      path: '/edit-loan/:username',
+      component: EditLoan
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+
+      path: '/AccountDetails/:username',
+
+      name: 'AccountDetails',
+
+      component: AccountDetails
+
+    },
+
+    {
+
+      path: '/UpdateDetails/:username',
+
+      name: 'UpdateDetails',
+
+      component: UpdateDetails
+
+    }
+  ],
+  mode: 'history'
+})
+
+new Vue({
+  router: spaRoutes,
+  render: h => h(App),
+}).$mount('#app')
